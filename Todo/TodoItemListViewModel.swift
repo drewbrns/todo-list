76fe4e7c71d2a5af691a3ebee10a8f5dd3cb11f3
Drew Barnes <djblin@gmail.com>
2021-09-13 10:06:29 +0000

    refactor: move TodoItemRepository into it's own file

--- Todo/TodoItemListViewModel.swift
+++ Todo/TodoItemListViewModel.swift
@@ -8,16 +8,6 @@
 import Foundation
 import Combine
 
-enum RepositoryError: Error {
-    case recordNotFound
-}
-
-protocol TodoItemRepository {
-    func load(completion: @escaping (Result<[TodoItem], Error>) -> Void)
-    func add(label: String, dueDate: Date, notes: String?) -> TodoItem
-    func remove(id: TodoItem.ID, completion: @escaping (Result<Void, Error>) -> Void)
-}
-
 final class TodoItemListViewModel: ObservableObject {
 
     private var list: ItemList
---
+++ Todo/TodoItemRepository.swift
@@ -0,0 +1,18 @@
+//
+//  TodoItemRepository.swift
+//  Todo
+//
+//  Created by Drew Barnes on 13/09/2021.
+//
+
+import Foundation
+
+enum RepositoryError: Error {
+    case recordNotFound
+}
+
+protocol TodoItemRepository {
+    func load(completion: @escaping (Result<[TodoItem], Error>) -> Void)
+    func add(label: String, dueDate: Date, notes: String?) -> TodoItem
+    func remove(id: TodoItem.ID, completion: @escaping (Result<Void, Error>) -> Void)
+}
