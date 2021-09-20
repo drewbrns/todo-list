dce1cd07d77c3b4fc36f9a9544a6c02956ce468d
Drew Barnes <djblin@gmail.com>
2021-09-20 01:32:21 +0000

    feat: add functionality to store item into database

---
+++ Todo/Presentation/AddTodoItemViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  AddTodoViewModel.swift
+//  Todo
+//
+//  Created by Drew Barnes on 19/09/2021.
+//
+
+import Foundation
+
+final class AddTodoItemViewModel {
+    private var repository: TodoItemCreateRepository!
+
+    @Published private(set) var onAddItem: TodoItem?
+    @Published private(set) var onError: Error?
+
+    init(repository: TodoItemCreateRepository) {
+        self.repository = repository
+    }
+
+    func addTodo(label: String, dueDate: Date = .distantFuture, notes: String? = nil) {
+        repository.add(label: label, dueDate: dueDate, notes: notes) { [weak self] result in
+            switch result {
+            case .success(let item):
+                self?.onAddItem = item
+            case .failure(let error):
+                self?.onError = error
+            }
+        }
+    }
+}
---
+++ TodoTests/AddTodoItemViewModelTests.swift
@@ -0,0 +1,44 @@
+//
+//  AddTodoViewModel.swift
+//  TodoTests
+//
+//  Created by Drew Barnes on 19/09/2021.
+//
+
+import XCTest
+@testable import Todo
+
+class AddTodoItemViewModelTests: XCTestCase {
+
+    func test_addTodo_createsAndReturnsTodoItem() {
+        let sut = makeSut().sut
+
+        XCTAssertNil(sut.onAddItem)
+        sut.addItem(label: "todo #1", dueDate: Date(), notes: "a good note")
+
+        XCTAssertNotNil(sut.onAddItem)
+    }
+
+    // MARK: Helpers
+    func makeSut() -> (sut: AddTodoItemViewModel, repo: TodoItemRepositoryStub) {
+        let repo = TodoItemRepositoryStub()
+        let vm = AddTodoItemViewModel(repository: repo)
+        return (vm, repo)
+    }
+
+    final class TodoItemRepositoryStub: TodoItemCreateRepository {
+        func add(
+            label: String,
+            dueDate: Date,
+            notes: String?,
+            completion: @escaping (Result<TodoItem, Error>) -> Void
+        ) {
+            let item = TodoItem(
+                label: label,
+                dueDate: dueDate,
+                notes: notes
+            )
+            completion(.success(item))
+        }
+    }
+}
