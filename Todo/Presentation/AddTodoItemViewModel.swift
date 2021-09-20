71c3c77cfbea2525a754d11b4bd560308c8990fb
Drew Barnes <djblin@gmail.com>
2021-09-20 10:51:24 +0000

    feat: add validation to todo item fields in viewmodel

--- Todo/Presentation/AddTodoItemViewModel.swift
+++ Todo/Presentation/AddTodoItemViewModel.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
+struct AddoTodoItemLabelRequiredError: Error & Exception {
+    var message: String {
+        "Todo field is required"
+    }
+}
+
+struct AddoTodoItemDueDateRequiredError: Error & Exception {
+    var message: String {
+        "Due Date field is required"
+    }
+}
+
+struct AddoTodoItemDueDateInvalidError: Error & Exception {
+    var message: String {
+        "Due Date must not be in the past"
+    }
+}
+
 final class AddTodoItemViewModel {
     private var repository: TodoItemCreateRepository!
 
@@ -17,14 +35,47 @@ final class AddTodoItemViewModel {
         self.repository = repository
     }
 
-    func addTodo(label: String, dueDate: Date, notes: String? = nil) {
-        repository.add(label: label, dueDate: dueDate, notes: notes) { [weak self] result in
-            switch result {
-            case .success(let item):
-                self?.onAddItem = item
-            case .failure(let error):
-                self?.onError = error
+    func addTodo(label: String?, dueDate: Date?, notes: String? = nil) {
+
+        do {
+            let aLabel = try validate(label: label)
+            let aDueDate = try validate(dueDate: dueDate)
+            let futureDueDate = try ensureDueDateIsInTheFuture(dueDate: aDueDate)
+
+            repository.add(label: aLabel, dueDate: futureDueDate, notes: notes) { [weak self] result in
+                switch result {
+                case .success(let item):
+                    self?.onAddItem = item
+                case .failure(let error):
+                    self?.onError = error
+                }
             }
+        } catch let error {
+            self.onError = error
         }
+    }
+
+    private func validate(label: String?) throws -> String {
+        guard let label = label else {
+            throw AddoTodoItemLabelRequiredError()
+        }
+        return label
+    }
+
+    private func validate(dueDate: Date?) throws -> Date {
+        guard let dueDate = dueDate else {
+            throw AddoTodoItemDueDateRequiredError()
+        }
+        return dueDate
+    }
+
+    private func ensureDueDateIsInTheFuture(dueDate: Date) throws -> Date {
+        let today = Date()
+
+        guard dueDate >= today else {
+            throw AddoTodoItemDueDateInvalidError()
+        }
+
+        return dueDate
     }
 }
--- TodoTests/AddTodoItemViewModelTests.swift
+++ TodoTests/AddTodoItemViewModelTests.swift
@@ -12,10 +12,42 @@ class AddTodoItemViewModelTests: XCTestCase {
 
     func test_addTodo_createsAndReturnsTodoItem() {
         let sut = makeSut().sut
+        let traveller = TimeTraveler()
+        traveller.travel(by: 60 * 60)
 
         XCTAssertNil(sut.onAddItem)
-        sut.addTodo(label: "todo #1", dueDate: Date(), notes: "a good note")
+        sut.addTodo(label: "todo #1", dueDate: traveller.generateDate(), notes: "a good note")
         XCTAssertNotNil(sut.onAddItem)
+    }
+
+    func test_addTodo_withInvalidTodoLabelInput_errors() {
+        let sut = makeSut().sut
+        XCTAssertNil(sut.onError)
+
+        sut.addTodo(label: nil, dueDate: Date(), notes: "a good note")
+
+        XCTAssertNotNil(sut.onError)
+        XCTAssertTrue(sut.onError is AddoTodoItemLabelRequiredError)
+    }
+
+    func test_addTodo_withInvalidDueDateInput_errors() {
+        let sut = makeSut().sut
+        XCTAssertNil(sut.onError)
+
+        sut.addTodo(label: "todo #1", dueDate: nil, notes: "a good note")
+
+        XCTAssertNotNil(sut.onError)
+        XCTAssertTrue(sut.onError is AddoTodoItemDueDateRequiredError)
+    }
+
+    func test_addTodo_errorsWhenDueDateIsInThePast() {
+        let sut = makeSut().sut
+        XCTAssertNil(sut.onError)
+
+        sut.addTodo(label: "todo #1", dueDate: .distantPast, notes: "a good note")
+
+        XCTAssertNotNil(sut.onError)
+        XCTAssertTrue(sut.onError is AddoTodoItemDueDateInvalidError)
     }
 
     // MARK: Helpers
@@ -38,6 +70,18 @@ class AddTodoItemViewModelTests: XCTestCase {
                 notes: notes
             )
             completion(.success(item))
+        }
+    }
+
+    final class TimeTraveler {
+        private var date = Date()
+
+        func travel(by timeInterval: TimeInterval) {
+            date = date.addingTimeInterval(timeInterval)
+        }
+
+        func generateDate() -> Date {
+            return date
         }
     }
 }
